64ed785a3f6d9db2d3ffc4dd420d48926f1dc5eb
ulloav <vulloa@csumb.edu>
2016-06-02 14:46:59 -0700

    added class descriptions to coreMLO.php page, added CST300 documents

--- coreMLO.php
+++ coreMLO.php
@@ -32,7 +32,19 @@
                 	Professional and Communication Development<br />
                     <span>
                     	<ul>
-                        	<li><a href="">CST 300: ProSeminar</a></li>
+                            <div class="classDiv">
+                        	    <li><span>CST 300: ProSeminar</span></li>
+                                <ul><li class="csumbSchool">CSUMB</li></ul>
+                                <p><span>Description:</span></p>
+                                <p>Helps students identify and articulate personal, professional, and social goals. Provides an integrated overview of the computer science and communication design majors and their requirements. Students develop a plan for their learning goals. Students learn writing, presentation, research and critical-thinking skills within the diversified fields of information technology and communication design. Students learn how to analyze, predict, and articulate trends in the academic, public service</p>
+                                <p><span>Example Coursework:</span></p>
+                                <p>
+                                    <ul>
+                                        <li>Industry Analysis Paper</li>
+                                        <li>Ethics Paper</li>
+                                    </ul>
+                                </p>
+                            </div>
                         </ul>
                     </span>
                 </dd>
@@ -40,9 +52,21 @@
                 	Programming and Problem Solving
                     <span>
                     	<ul>
-                        	<li>CST 231: Problem Solving & Programming C++</li>
-                            <li>CST 238: Introduction to Data Structures</li>
-                            <ul><li class="otherSchool">CO SCI 440 @ Los Angeles Mission College</li></ul>
+                            <div class="classDiv">
+                        	    <li>CST 231: Problem Solving & Programming C++</li>
+                                <p><span>Description:</span></p>
+                                <p>This is an introductory programming course to develop problem-solving techniques for numerical and non-numerical problems from various disciplines. Students will design the solution to each problem and implement it in the C++ programming language. Coverage includes introduction to computer system, C++ basics, flow control, functions, I/O streams, arrays, strings, and classes.</p>
+                                <p><span>Example Coursework:</span></p>
+                                <p></p>
+                            </div>
+                            <div class="classDiv">
+                                <li>CST 238: Introduction to Data Structures</li>
+                                <ul><li class="otherSchool">CO SCI 440 @ Los Angeles Mission College</li></ul>
+                                <p><span>Description:</span></p>
+                                <p>Provides students with intermediate-level programming skills to make it possible for them to write clear, efficient, and high-quality code. Students learn basic data structures such as array, list, stack, queue, and tree. They also acquire several basic algorithms in programming such as searching, sorting, and recursion. Furthermore, students learn in-depth knowledge of C++ programming language such as classes, pointers, and dynamic memory management.</p>
+                                <p><span></span></p>
+                                <p></p>
+                            </div>
                         </ul>
                     </span>
                 </dd>
@@ -50,14 +74,34 @@
                 	Mathematics for Computing
                     <span>
                     	<ul>
-                        	<li>MATH 170: Discrete Math</li>
-                            <ul><li class="otherSchool">CSIS 213 @ MSJC</li></ul>
-                            <li>MATH 150: Calculus I</li>
-                            <ul><li class="otherSchool">Math 211 @ College of the Canyons</li></ul>
-                            <li>MATH 151: Calculus II</li>
-                            <ul><li class="otherSchool">Math 266 @ Los Angeles Valley College</li></ul>
-                            <li>MATH 370: Logic & Computation</li>
-                            <ul><li class="otherSchool"></li></ul>
+                            <div class="classDiv">
+                                <li>MATH 170: Discrete Math</li>
+                                <ul><li class="otherSchool">CSIS 213 @ MSJC</li></ul>
+                                <p><span>Description:</span></p>
+                                <p>Includes sets and sequences, elementary logic, relations, induction, counting principles, discrete probability, Boolean algebra, logic networks, matrices, graph theory, and trees. Applies these topics to real life and branches of science, particularly computer science.</p>
+                                <p><span></span></p>
+                                <p></p>
+                            </div>
+                            <div class="classDiv">
+                                <li>MATH 150: Calculus I</li>
+                                <ul><li class="otherSchool">Math 211 @ College of the Canyons</li></ul>
+                                <p><span>Description:</span></p>
+                                <p>Includes limits, continuity, derivatives including trigonometric functions, chain rule, curve sketching, extremum problems, implicit differentiation, related rates, Mean Value Theorem, logarithmic and trigonometric functions, introduction to integration, fundamental theorem of calculus, substitution, and applications.</p>
+                            </div>
+                            <div class="classDiv">
+                                <li>MATH 151: Calculus II</li>
+                                <ul><li class="otherSchool">Math 266 @ Los Angeles Valley College</li></ul>
+                                <p><span>Description:</span></p>
+                                <p>Includes the calculus of exponential and logarithmic functions, trigonometric and inverse trigonometric functions, techniques of integration, separable differential equations, Taylor polynomials, L'Hôpital's rule, improper integrals, series, and introduction to partial derivatives.</p>
+                            </div>
+                            <div class="classDiv">
+                                <li>MATH 370: Logic & Computation</li>
+                                <ul><li class="otherSchool"></li></ul>
+                                <p><span>Description:</span></p>
+                                <p>An advanced study of logic and discrete structures that have application in computer science. Includes logic, propositional and predicate calculus, proof structures, algorithms, and complexity. Emphasizes applications to core areas of computer science, such as computer architecture, programming languages, the theory of computation, artificial intelligence, software engineering, and relational databases.</p>
+                                <p><span></span></p>
+                                <p></p>
+                            </div>
                         </ul>
                     </span>
                 </dd>
@@ -65,7 +109,12 @@
                 	Knowledge of Computers and Networks
                     <span>
                     	<ul>
-                        	<li>CST 237: Computer Architecture</li>
+                            <div class="classDiv">
+                        	    <li>CST 237: Computer Architecture</li>
+                                <ul><li class="csumbSchool">CSUMB - Summer 2016</li></ul>
+                                <p><span>Description:</span></p>
+                                <p>This course introduces computer architectures and computer systems management. Includes data representation, memory, registers, and internals of common computers and system interfaces. Knowledge of C/C++ programming helpful but not mandatory.</p>
+                            </div>
                         </ul>
                     </span>
                 </dd>
@@ -73,7 +122,14 @@
                 	Interaction Design
                     <span>
                     	<ul>
-                        	<li>CST 205: Multimedia Design & Prog</li>
+                            <div class="classDiv">
+                        	    <li>CST 205: Multimedia Design & Prog</li>
+                                <ul><li class="csumbSchool">CSUMB</li></ul>
+                                <p><span>Description:</span></p>
+                                <p>Introduces design, creation, and manipulation of interactive applications and electronic media for communication purpose. Focuses on creating media, understanding media concepts, and manipulating the created media using basic programming concepts of control flow, functions, expressions and data types in the Python language. Students acquire a basic understanding for digital media formats, how to design, create such media using basic programming skills.</p>
+                                <p><span>Example Coursework:</span></p>
+                                <p></p>
+                            </div>
                         </ul>
                     </span>
                 </dd>
